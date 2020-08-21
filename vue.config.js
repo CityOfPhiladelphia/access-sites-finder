ac142657670204a8adce7e96d41938703d093a09
Karissa <k@karissademi.com>
2020-08-21 11:58:39 -0400

    Update dev deploy location

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   filenameHashing: false,
-  publicPath: "/access-centers/",
+  ///publicPath: "/access-centers/",
+  publicPath: "access-centers/dev/",
   chainWebpack: (config) => {
     config.resolve.symlinks(false);
   },
